5fde67a00c0772e49de5fd47981f8a5151d305fc
Giancarlo <giancarloverri@hotmail.com>
2021-04-01 13:56:41 -0300

    Exception handling for delete (referential integrity) Checklist:

--- Projeto_ASPNET/Services/SellerService.cs
+++ Projeto_ASPNET/Services/SellerService.cs
@@ -35,9 +35,17 @@ namespace Projeto_ASPNET.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException)
+            {
+                throw new IntegrityException("Can't this seller because he/she has sales!");
+            }
+            
         }
 
         public async Task UpdateAsync(Seller obj)
